681211acd399d6b199f1516516e2e8e30155089f
andreiporumb <37443731+andreiporumb@users.noreply.github.com>
2018-08-21 10:20:29 +0300

    First unit tests defined

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        [Fact]
+        public void TestExistence()
+        {
+            int var=1;
+            Assert.Equal(1, var);
+        }
+
+        private int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void ThisIsATheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        private bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
